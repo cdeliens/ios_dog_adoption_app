a5ac61e0b58f3e468e481c0da89defb4d51c927d
cdeliens <cdeliens@gmail.com>
2017-03-16 14:40:08 -0600

    last changes

--- Dog Adoption/DogsViewController.m
+++ Dog Adoption/DogsViewController.m
@@ -37,9 +37,6 @@
     return self.dogsArray.count;
 }
 
-// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     DogTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[DogTableViewCell getClassName]];
